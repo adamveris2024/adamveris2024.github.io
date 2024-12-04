a6b5aaa1b772eadf42de91f9cf32afc67acfa8bd
Adam Verissimo <adamveris2024@gmail.com>
2024-12-03 18:48:31 -0800

    finished initial values

---
+++ script.js
@@ -0,0 +1,7 @@
+// Game state variables
+let currentPlayer = 'X'; // this line makes it so X always goes first
+let gameBoard = ['', '', '', '', '', '', '', '', '']; // this sets the board state to 9 empty squares
+let gameActive = true; // this is telling the computer whether the game is active or not
+let xWins = 0; // this line is setting the initial value for xWins
+let oWins = 0; // this line is setting the initial value for oWins
+let ties = 0; // this line is setting the initial value for ties
